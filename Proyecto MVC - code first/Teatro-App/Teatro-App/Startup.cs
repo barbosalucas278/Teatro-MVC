c10aceba82bf6258573019d1029633f98f6e987c
barbosalucas278 <barbosalucasroberto@gmail.com>
2021-04-08 20:21:32 -0300

    Resolves Issue #1

---
+++ Proyecto MVC - code first/Teatro-App/Teatro-App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Teatro_App.Startup))]
+namespace Teatro_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
